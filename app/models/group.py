e99de90c77a5ada07e331ca13991494618cbc8d1
Michael Pisman <mpisman@ucmerced.edu>
2022-11-01 01:16:34 -0700

    changed owner from User to PydanticObjectId

--- app/models/group.py
+++ app/models/group.py
@@ -4,7 +4,9 @@ from pydantic import BaseModel, Field
 from app.models.user import User
 
 class Group(Document):
-    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
+    # id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
+    name: str = Field(...)
+    # owner: User = Field(...)
+    owner: PydanticObjectId = Field(...)
     users:  List[PydanticObjectId] = []
     admins: List[PydanticObjectId] = []
-    owner:  User
