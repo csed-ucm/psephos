168943ca9386ddbc6bcaff5ea5256486396abd8a
Michael Pisman <mpisman@ucmerced.edu>
2022-10-31 21:34:18 -0700

    Fixed bug "no Group class"

--- app/models/group.py
+++ app/models/group.py
@@ -1,9 +1,9 @@
 from typing import List
-from beanie import PydanticObjectId
+from beanie import PydanticObjectId, Document
 from pydantic import BaseModel, Field
 from app.models.user import User
 
-class Group():
+class Group(Document):
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
     users:  List[PydanticObjectId] = []
     admins: List[PydanticObjectId] = []
