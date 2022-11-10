158db538dbf9509534df952bc4fc3b41c82cf693
Michael Pisman <mpisman@ucmerced.edu>
2022-11-09 23:11:30 -0800

    Added add_user() to Group model

--- app/models/group.py
+++ app/models/group.py
@@ -10,3 +10,25 @@ class Group(Document):
     owner: PydanticObjectId = Field(...)
     users:  List[PydanticObjectId] = []
     admins: List[PydanticObjectId] = []
+
+    # Add user to group 
+    async def add_user(self, user_id: PydanticObjectId, role: str):
+        
+        # Check if user is already in group
+        if user_id in self.users:
+            # Return 400 with error message
+            raise 
+        
+        if role == "admin":
+            self.admins.append(user_id)
+        self.users.append(user_id)
+        await self.save()  
+    # Remove user from group
+
+    # Delete user from all groups
+    # def remove_user(self, user_id: PydanticObjectId):
+    #     if user_id in group.users:
+    #         group.users.remove(user_id)
+    #     if user_id in group.admins:
+    #         group.admins.remove(user_id)
+    #     group.save()
