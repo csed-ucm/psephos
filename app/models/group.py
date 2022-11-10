f23a5f529cab62ac00f23d1cbf96cf3635d54885
Michael Pisman <mpisman@ucmerced.edu>
2022-11-10 01:54:06 -0800

    Fixed missing type

--- app/models/group.py
+++ app/models/group.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from app.models.user import User
 
 class Group(Document):
-    # id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
     name: str = Field(...)
     # owner: User = Field(...)
     owner: PydanticObjectId = Field(...)
