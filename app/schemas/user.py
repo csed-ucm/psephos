b179755175fe9d4029055099a8e93f5af153672f
Michael Pisman <mpisman@ucmerced.edu>
2022-11-11 22:12:20 -0800

    Updated User schema to store first_name, last_name

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,13 +1,14 @@
 from beanie import PydanticObjectId
 from fastapi_users import schemas
-
+from pydantic import Field
 
 class UserRead(schemas.BaseUser[PydanticObjectId]):
-    pass
-
+    first_name: str = Field(default_factory=str, max_length=20, min_length=2, regex="^[A-Z][a-z]*$")
+    last_name: str = Field(default_factory=str, max_length=20, min_length=2, regex="^[A-Z][a-z]*$")
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    first_name: str = Field(default_factory=str, max_length=20, min_length=2, regex="^[A-Z][a-z]*$")
+    last_name: str = Field(default_factory=str, max_length=20, min_length=2, regex="^[A-Z][a-z]*$")
 
 
 class UserUpdate(schemas.BaseUserUpdate):
