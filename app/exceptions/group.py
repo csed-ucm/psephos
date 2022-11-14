dc454e46d558c582c56ee4a57eeccda8d1df639f
Michael Pisman <mpisman@ucmerced.edu>
2022-11-13 23:52:31 -0800

    Updated names of exceptions, Added GroupNotFound

--- app/exceptions/group.py
+++ app/exceptions/group.py
@@ -7,7 +7,7 @@ from app.models.group import Group
 from app.utils.colored_dbg import print_warning
 
 # If user already exists in the group
-class user_exists(HTTPException):
+class UserAlreadyExists(HTTPException):
     def __init__(self, user: User, group: Group):
         super().__init__(status_code=400, detail=f"User {user.email} already exists in group {group.name}") 
 
@@ -16,9 +16,17 @@ class user_exists(HTTPException):
         # return self.P
 
 # Not authorized
-class user_not_authorized(HTTPException):
+class UserNotAuthorized(HTTPException):
     def __init__(self, user: User, group: Group, action: str = "perform this action"):
         super().__init__(status_code=403, detail=f"User {user.email} is not authorized to {action} in group {group.name}") 
+
+    def __str__(self):
+        print_warning(self.detail)
+
+# Group not found
+class GroupNotFound(HTTPException):
+    def __init__(self, group_id: PydanticObjectId):
+        super().__init__(status_code=404, detail=f"Group with id {group_id} not found") 
 
     def __str__(self):
         print_warning(self.detail)
