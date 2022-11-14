423acd78f122cf4a7859821e1eef2c09a2857cc4
Michael Pisman <mpisman@ucmerced.edu>
2022-11-13 23:53:47 -0800

    Cleaned Group model Added field and basic validation

--- app/models/group.py
+++ app/models/group.py
@@ -5,30 +5,8 @@ from app.models.user import User
 
 class Group(Document):
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
-    name: str = Field(...)
-    # owner: User = Field(...)
-    owner: PydanticObjectId = Field(...)
+    name: str = Field(default="", min_length=3, max_length=50, regex=r'^[A-Z][A-Za-z]{2,}(\s(\d+|[A-Z][A-Za-z]*))*$')   
+    description: str = Field(default="", title="Description", max_length=300)
+    owner: PydanticObjectId = Field(default_factory=PydanticObjectId, title="ownerID", description="Owner of the group")
     users:  List[PydanticObjectId] = []
     admins: List[PydanticObjectId] = []
-
-    # Add user to group 
-    async def add_user(self, user_id: PydanticObjectId, role: str):
-        
-        # Check if user is already in group
-        if user_id in self.users:
-            # Return 400 with error message
-            raise 
-        
-        if role == "admin":
-            self.admins.append(user_id)
-        self.users.append(user_id)
-        await self.save()  
-    # Remove user from group
-
-    # Delete user from all groups
-    # def remove_user(self, user_id: PydanticObjectId):
-    #     if user_id in group.users:
-    #         group.users.remove(user_id)
-    #     if user_id in group.admins:
-    #         group.admins.remove(user_id)
-    #     group.save()
