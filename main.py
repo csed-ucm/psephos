d86460b19eca72ff3695030596e77fa553abb25f
Michael Pisman <mpisman@ucmerced.edu>
2022-12-02 09:27:41 -0800

    Added reloading to uvicorn

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import uvicorn
 import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.app:app", reload=True, debug=True, host="0.0.0.0", port=8000)
