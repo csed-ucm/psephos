70c821a0b2dd077dc09a25096277da6b3b8f4081
Michael Pisman <mpisman@ucmerced.edu>
2022-12-02 09:29:54 -0800

    Updated UserNotFound to print different messages

--- app/exceptions/user.py
+++ app/exceptions/user.py
@@ -1,13 +1,24 @@
 from fastapi import HTTPException
+from fastapi.logger import logger
 from beanie import PydanticObjectId
+from pydantic import EmailStr, Field
 from app.models.user import User
 from app.models.group import Group
 from app.utils.colored_dbg import print_warning
 
 
 class UserNotFound(HTTPException):
-    def __init__(self, user_id: PydanticObjectId):
-        super().__init__(status_code=404, detail=f"User with id {user_id} not found") 
+    def __init__(self, user: PydanticObjectId | str | None = None):
+        message = f"User not found"  # Default message
+        if user:
+            if user.__class__ == PydanticObjectId:
+                message = f"User with id {user} not found"
+            elif user.__class__ == str:
+                message = f"User with email {user} not found"
+            # elif user.__class__ == str:
+            #     message = f"User with name {user} not found"
+        super().__init__(status_code=404, detail=message)
 
     def __str__(self):
-        print_warning(self.detail)
+        logger.warning(self.detail)
+        # print_warning(self.detail)
