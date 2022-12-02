ae4a4035ced81cfa366011e794401ca8988f6659
Michael Pisman <mpisman@ucmerced.edu>
2022-12-02 09:35:19 -0800

    Updated new group schemas

--- app/schemas/group.py
+++ app/schemas/group.py
@@ -1,8 +1,12 @@
 from beanie import PydanticObjectId
-from app.models.group import Group
 from pydantic import BaseModel, Field
 from typing import List
 from random import choice, randint
+from fastapi import Response
+from app.models.group import Group
+from app.models.user import User
+from app.schemas.user import UserAddToGroup
+
 
 class GroupList(BaseModel):
     groups: List[Group]
@@ -11,21 +15,47 @@ class GroupList(BaseModel):
 class GroupRead(BaseModel):
     name: str
     description: str
-    owner: str
+    owner_name: str
+    owner_email: str
 
-    class Config:
-            schema_extra = {
-                "example": {
-                    "name": "Foo",
-                    "description": "A very nice Group"
-                }
-            }
             
-class GroupCreate(BaseModel):
-    name: str = Field(default="", min_length=3, max_length=50, regex=r'^[A-Z][A-Za-z]{2,}(\s(\d+|[A-Z][A-Za-z]*))*$')
+class GroupCreateIn(BaseModel):
+    name: str = Field(default="", min_length=3, max_length=50, regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
     description: str = Field(default="", title="Description", max_length=300)
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Group 01",
+                "description": "My first Group",
+            }
+        }
     
-# class groupUpdate(BaseModel):
-#     name: str
-#     description: str
+class GroupCreateOut(BaseModel):
+    message: str = "Group created successfully"
+    name: str
+    # description: str
+    
+class GroupUpdateIn(BaseModel):
+    name: str = Field(default="", min_length=3, max_length=50, regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
+    description: str = Field(default="", title="Description", max_length=300)
 
+class GroupUpdateOut(Response):
+    status_code = 200
+    media_type = "application/json"
+    content = {"message": "Group updated successfully"} 
+
+class GroupAddMembers(BaseModel):
+    members: List[UserAddToGroup]
+    class Config:
+        schema_extra = {
+            "example": {
+                "members": [
+                    {"email": "user1@example.com", "role": "admin"},
+                    {"email": "user2@example.com", "role": "user"},
+                ]
+            }
+        }
+    
+
+class GroupReadMembers(BaseModel):
+    members: List[User]
