1723b3395cc8040bf594084999fee3630386eb5c
Michael Pisman <mpisman@ucmerced.edu>
2022-12-16 01:11:27 -0800

    Updated Group schemas:  - Added GroupReadSimple  - Added GroupReadFull  - Added GroupMember schema  - Updated GroupReadMembers  - Added examples

--- app/schemas/group.py
+++ app/schemas/group.py
@@ -8,17 +8,49 @@ from app.models.user import User
 from app.schemas.user import UserAddToGroup
 
 
-class GroupList(BaseModel):
-    groups: List[Group]
+class GroupReadSimple(BaseModel):
+    name: str = Field(default="No Name", min_length=3, max_length=50, regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
+    role: str = Field(default="user", title="Role")
+
+    class Config:
+        schema_extra = {
+            "user example": {
+                "name": "Group 01",
+                "role": "user",
+            },
+            "admin example": {
+                "name": "Group 01",
+                "role": "admin",
+            }
+        }
 
 
-class GroupRead(BaseModel):
+class GroupReadFull(BaseModel):
     name: str
     description: str
     owner_name: str
     owner_email: str
+    # TODO: Add list of members (emails with roles)
 
-            
+
+class GroupList(BaseModel):
+    groups: List[GroupReadSimple]       
+    
+    class Config:
+        schema_extra = {
+            "List of groups": {
+                "groups": [
+                    {"name": "Group 01", "role": "user"},
+                    {"name": "Group 02", "role": "user"},
+                    {"name": "Group 03", "role": "admin"},
+                ]
+            },
+            "Empty list of groups": {  
+                "groups": []
+            }
+        }   
+
+
 class GroupCreateIn(BaseModel):
     name: str = Field(default="", min_length=3, max_length=50, regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
     description: str = Field(default="", title="Description", max_length=300)
@@ -44,6 +76,21 @@ class GroupUpdateOut(Response):
     media_type = "application/json"
     content = {"message": "Group updated successfully"} 
 
+class GroupMember(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+    role: str = Field(default="user", title="Role")
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "user@example.com",
+                "first_name": "John",
+                "last_name": "Doe",
+                "role": "user",
+            }
+        }
+
 class GroupAddMembers(BaseModel):
     members: List[UserAddToGroup]
     class Config:
@@ -55,7 +102,25 @@ class GroupAddMembers(BaseModel):
                 ]
             }
         }
-    
-
+        
 class GroupReadMembers(BaseModel):
-    members: List[User]
+    members: List[GroupMember]
+    class Config:
+        schema_extra = {
+            "example": {
+                "members": [
+                    {
+                        "email": "jdoe@example.com", 
+                        "first_name": "John",
+                        "last_name": "Doe",
+                        "role": "admin"
+                    },
+                    {
+                        "email": "jsmith@example.com", 
+                        "first_name": "Jack",
+                        "last_name": "Smith",
+                        "role": "user"
+                    }
+                ]
+            }
+        }
