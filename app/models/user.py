f71ec1a8f2c403e2c003cd777ce6f9654523d0a4
Michael Pisman <mpisman@ucmerced.edu>
2022-12-17 21:40:58 -0800

    Cleaned files for mypy and flake8

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,20 @@
-from typing import List, Dict
+from typing import List
 from beanie import PydanticObjectId
-from fastapi_users.db import BeanieBaseUser, BeanieUserDatabase
+from fastapi_users.db import BeanieBaseUser
 from pydantic import Field
-from beanie.operators import In
+
 
 class User(BeanieBaseUser[PydanticObjectId]):
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
     # groups: Dict[List[PydanticObjectId], str] = {}
     groups: List[PydanticObjectId] = []
-    first_name: str = Field(default_factory=str, max_length=20, min_length=2, regex="^[A-Z][a-z]*$")
-    last_name: str = Field(default_factory=str, max_length=20, min_length=2, regex="^[A-Z][a-z]*$") 
-
-
+    first_name: str = Field(
+        default_factory=str,
+        max_length=20,
+        min_length=2,
+        regex="^[A-Z][a-z]*$")
+    last_name: str = Field(
+        default_factory=str,
+        max_length=20,
+        min_length=2,
+        regex="^[A-Z][a-z]*$")
