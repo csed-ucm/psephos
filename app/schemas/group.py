1a72a4d82f45105d05466529c658ed459c78fc41
Michael Pisman <mpisman@ucmerced.edu>
2022-12-17 22:15:50 -0800

    Updated response models, cleaned files for mypy and flake8

--- app/schemas/group.py
+++ app/schemas/group.py
@@ -1,15 +1,19 @@
 from beanie import PydanticObjectId
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from typing import List
-from random import choice, randint
+# from random import choice, randint
 from fastapi import Response
-from app.models.group import Group
-from app.models.user import User
+# from app.models.group import Group
+# from app.models.user import User
 from app.schemas.user import UserAddToGroup
 
 
 class GroupReadSimple(BaseModel):
-    name: str = Field(default="No Name", min_length=3, max_length=50, regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
+    name: str = Field(
+        default="No Name",
+        min_length=3,
+        max_length=50,
+        regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
     role: str = Field(default="user", title="Role")
 
     class Config:
@@ -29,13 +33,13 @@ class GroupReadFull(BaseModel):
     name: str
     description: str
     owner_name: str
-    owner_email: str
+    owner_email: EmailStr
     # TODO: Add list of members (emails with roles)
 
 
 class GroupList(BaseModel):
-    groups: List[GroupReadSimple]       
-    
+    groups: List[GroupReadSimple]
+
     class Config:
         schema_extra = {
             "List of groups": {
@@ -45,15 +49,20 @@ class GroupList(BaseModel):
                     {"name": "Group 03", "role": "admin"},
                 ]
             },
-            "Empty list of groups": {  
+            "Empty list of groups": {
                 "groups": []
             }
-        }   
+        }
 
 
 class GroupCreateIn(BaseModel):
-    name: str = Field(default="", min_length=3, max_length=50, regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
+    name: str = Field(
+        default="",
+        min_length=3,
+        max_length=50,
+        regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
     description: str = Field(default="", title="Description", max_length=300)
+
     class Config:
         schema_extra = {
             "example": {
@@ -61,26 +70,36 @@ class GroupCreateIn(BaseModel):
                 "description": "My first Group",
             }
         }
-    
+
+
 class GroupCreateOut(BaseModel):
     message: str = "Group created successfully"
+    id: PydanticObjectId
     name: str
     # description: str
-    
+
+
 class GroupUpdateIn(BaseModel):
-    name: str = Field(default="", min_length=3, max_length=50, regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
+    name: str = Field(
+        default="",
+        min_length=3,
+        max_length=50,
+        regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
     description: str = Field(default="", title="Description", max_length=300)
+
 
 class GroupUpdateOut(Response):
     status_code = 200
     media_type = "application/json"
-    content = {"message": "Group updated successfully"} 
+    content = {"message": "Group updated successfully"}
+
 
 class GroupMember(BaseModel):
-    email: str
+    email: EmailStr
     first_name: str
     last_name: str
     role: str = Field(default="user", title="Role")
+
     class Config:
         schema_extra = {
             "example": {
@@ -91,8 +110,10 @@ class GroupMember(BaseModel):
             }
         }
 
+
 class GroupAddMembers(BaseModel):
     members: List[UserAddToGroup]
+
     class Config:
         schema_extra = {
             "example": {
@@ -102,21 +123,23 @@ class GroupAddMembers(BaseModel):
                 ]
             }
         }
-        
+
+
 class GroupReadMembers(BaseModel):
     members: List[GroupMember]
+
     class Config:
         schema_extra = {
             "example": {
                 "members": [
                     {
-                        "email": "jdoe@example.com", 
+                        "email": "jdoe@example.com",
                         "first_name": "John",
                         "last_name": "Doe",
                         "role": "admin"
                     },
                     {
-                        "email": "jsmith@example.com", 
+                        "email": "jsmith@example.com",
                         "first_name": "Jack",
                         "last_name": "Smith",
                         "role": "user"
