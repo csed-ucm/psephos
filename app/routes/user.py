660de6b5f9272e969f944954881a3580afc710fe
Michael Pisman <mpisman@ucmerced.edu>
2022-12-17 21:43:48 -0800

    Changed the response of list_user_groups(), cleaned the file

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 from app.models.user import User
 from app.models.user_manager import current_active_user
 from beanie import PydanticObjectId
@@ -6,7 +7,7 @@ from app.exceptions import user as user_exceptions
 from app.utils.user import get_user_groups
 
 
-#APIRouter creates path operations for user module
+# APIRouter creates path operations for user module
 router = APIRouter(
     prefix="/users",
     tags=["Users"],
@@ -15,13 +16,17 @@ router = APIRouter(
 
 
 # List all groups that a user is a member of
-@router.get("/groups", response_description="List all groups that a user is a member of")
-async def list_user_groups(user_id: PydanticObjectId | None = None, user: User = Depends(current_active_user)):
+@router.get("/groups",
+            response_description="List all groups that a user is a member of")
+async def list_user_groups(user_id: PydanticObjectId | None = None,
+                           user: User = Depends(current_active_user)) -> JSONResponse:
     if user_id:
         search = await User.find_one({"_id": user_id})
         if search:
-            user = search    
+            user = search
         else:
             raise user_exceptions.UserNotFound(user_id)
-        
-    return await get_user_groups(user)
+
+    # TODO: Consider using a proper model
+    groups = await get_user_groups(user)
+    return JSONResponse(groups)
