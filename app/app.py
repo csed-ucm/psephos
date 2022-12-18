a0fb46dbd5f43d52c006a38c19a01672da5b302c
Michael Pisman <mpisman@ucmerced.edu>
2022-12-18 02:53:59 -0800

    Fixed import issue, cleaned the file

--- app/app.py
+++ app/app.py
@@ -1,25 +1,27 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from beanie import init_beanie
 from fastapi.middleware.cors import CORSMiddleware
 from app.mongo_db import mainDB
-from app import routes
+# import app.routes as routes
+from app.routes import group, user
 from app.config import get_settings
 from app.schemas.user import UserCreate, UserRead, UserUpdate
-from app.models.user_manager import auth_backend, current_active_user, fastapi_users
+from app.models.user_manager import auth_backend, fastapi_users
 from app.models.user import User
 from app.models.group import Group
 
 settings = get_settings()
-    
+
 app = FastAPI(
     title=settings.app_name,
     description="A REST API to manage users and polls",
     version=settings.app_version,
 )
 
-app.include_router(routes.group.router)
-app.include_router(routes.user.router)
-app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["Auth"])
+app.include_router(group.router)
+app.include_router(user.router)
+app.include_router(fastapi_users.get_auth_router(
+    auth_backend), prefix="/auth/jwt", tags=["Auth"])
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
@@ -51,13 +53,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-@app.get("/authenticated-route")
-async def authenticated_route(user: User = Depends(current_active_user)):
-    return {"message": f"Hello {user.email}!"}
+# @app.get("/authenticated-route")
+# async def authenticated_route(user: User = Depends(current_active_user)) -:
+#     return {"message": f"Hello {user.email}!"}
 
 
 @app.on_event("startup")
-async def on_startup():
+async def on_startup() -> None:
     await init_beanie(
         database=mainDB,
         document_models=[
