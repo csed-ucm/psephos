2e299df5841acf473fdc69038252edc347556ff9
Michael Pisman <mpisman@ucmerced.edu>
2023-02-07 01:43:52 +0000

    Created method to add member to the workspace list

--- app/models/workspace.py
+++ app/models/workspace.py
@@ -7,7 +7,7 @@ from app.schemas.workspace import WorkspaceID
 
 class Workspace(Document):
     """ Workspace model, based on Beanie Document class. This model is used to store information about workspaces.
-        The model also contains methods specific to the workspace. 
+        The model also contains methods specific to the workspace.
         The owner and members are stored as links to the User model, which can be fetched to get the user data.
 
     Parameters:
@@ -22,10 +22,19 @@ class Workspace(Document):
     name: str = Field(default="", min_length=3, max_length=50,
                       regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
     description: str = Field(default="", title="Description", max_length=300)
-    owner: Link[User] = Field(title="Owner", description="Owner of the workspace")
+    owner: Link[User] = Field(
+        title="Owner", description="Owner of the workspace")
     members: list[Link[User]] = []
 
     @after_event(Insert)
     async def workspace_created(self) -> None:
         colored_dbg.info(
             f'New workspace "{self.name}" ({self.id}) has been created')
+
+    async def add_member(self, user: User) -> None:
+        from app.mongo_db import create_link
+        link = await create_link(user)
+        self.members.append(link)
+        colored_dbg.info(
+            f'User {user.id} has been added to workspace {self.id} as a member.')
+        await self.save()
--- app/mongo_db.py
+++ app/mongo_db.py
@@ -1,4 +1,6 @@
+from beanie import Document, Link
 from typing import AsyncGenerator
+from bson import DBRef
 import motor.motor_asyncio
 from fastapi_users.db import BeanieUserDatabase
 from app.models.user import User
@@ -16,3 +18,11 @@ mainDB = client.app
 
 async def get_user_db() -> AsyncGenerator:
     yield BeanieUserDatabase(User)
+
+
+# Create a link to the Document model
+async def create_link(document: Document) -> Link:
+    ref = DBRef(collection=document._document_settings.name,  # type: ignore
+                id=document.id)
+    link = Link(ref, User)
+    return link
