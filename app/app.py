a27d8f84a105ad6c23e40364d241aaeef8f20bae
Michael Pisman <mpisman@ucmerced.edu>
2023-02-07 04:21:11 +0000

    Added Workspace router to the project Added Workspace document model to the database Added set_active_user Dependency to save user in the context Temporary removed user and group routers from the project

--- app/app.py
+++ app/app.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from beanie import init_beanie
 from fastapi.middleware.cors import CORSMiddleware
 from app.mongo_db import mainDB
 # import app.routes as routes
-from app.routes import group, user
+# from app.routes import group, user, workspace
+from app.routes import workspace
 from app.config import get_settings
 from app.schemas.user import UserCreate, UserRead, UserUpdate
-from app.models.user_manager import auth_backend, fastapi_users
+from app.models.user_manager import auth_backend, fastapi_users, get_current_active_user, current_active_user
+from app.models.workspace import Workspace
 from app.models.user import User
 from app.models.group import Group
-
 settings = get_settings()
+
+
+# Get all workspaces
+async def set_active_user(user_account: User = Depends(get_current_active_user)):
+    current_active_user.set(user_account)
+    return user_account
 
 app = FastAPI(
     title=settings.app_name,
@@ -18,30 +25,25 @@ app = FastAPI(
     version=settings.app_version,
 )
 
-app.include_router(group.router)
-app.include_router(user.router)
-app.include_router(fastapi_users.get_auth_router(
-    auth_backend), prefix="/auth/jwt", tags=["Auth"])
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["Auth"],
-)
-app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["Auth"],
-)
-app.include_router(
-    fastapi_users.get_verify_router(UserRead),
-    prefix="/auth",
-    tags=["Auth"],
-)
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["Users"],
-)
+app.include_router(workspace.router,
+                   prefix="/workspaces",
+                   tags=["Workspaces"],
+                   dependencies=[Depends(set_active_user)])
+app.include_router(fastapi_users.get_auth_router(auth_backend),
+                   prefix="/auth/jwt",
+                   tags=["Auth"])
+app.include_router(fastapi_users.get_register_router(UserRead, UserCreate),
+                   prefix="/auth",
+                   tags=["Auth"])
+app.include_router(fastapi_users.get_reset_password_router(),
+                   prefix="/auth",
+                   tags=["Auth"])
+app.include_router(fastapi_users.get_verify_router(UserRead),
+                   prefix="/auth",
+                   tags=["Auth"])
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate),
+                   prefix="/users",
+                   tags=["Users"])
 
 
 origins = ["*"]
@@ -53,9 +55,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-# @app.get("/authenticated-route")
-# async def authenticated_route(user: User = Depends(current_active_user)) -:
-#     return {"message": f"Hello {user.email}!"}
 
 
 @app.on_event("startup")
@@ -65,6 +64,7 @@ async def on_startup() -> None:
         document_models=[
             # BUG: Incompatible type "Type[Group]"; expected "Union[Type[View], str]"
             User,  # type: ignore
-            Group  # type: ignore
+            Group,  # type: ignore
+            Workspace
         ],
     )
