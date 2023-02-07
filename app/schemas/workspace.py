d34ce933123479d13f08309be73cb821f870a57f
Michael Pisman <mpisman@ucmerced.edu>
2023-02-07 01:52:35 +0000

    Created schemas for reading workspaces

--- app/schemas/workspace.py
+++ app/schemas/workspace.py
@@ -1,4 +1,7 @@
 from beanie import PydanticObjectId
+from pydantic import BaseModel, Field
+from typing import List
+from app.schemas.user import UserReadBasicInfo, UserRead
 
 
 # Custom PydanticObjectId class to override due to a bug
@@ -9,3 +12,51 @@ class WorkspaceID(PydanticObjectId):
             type="string",
             example="5eb7cf5a86d9755df3a6c593",
         )
+
+
+# Schema for the response with basic workspace info (name and role)
+class WorkspaceReadShort(BaseModel):
+    name: str = Field(title="Name")
+    description: str = Field(title="Description")
+    owner: bool = Field(title="Owner")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Workspace 01",
+                "description": "This is an example workspace",
+                "owner": "true",
+            }
+        }
+
+
+# Schema for the response with full workspace info (name, description, owner info)
+class WorkspaceReadFull(BaseModel):
+    name: str = Field(example="Workspace 01", title="Name")
+    description: str = Field(title="Description")
+    owner: UserRead = Field(title="Owner")
+
+
+# Schema for the response with a list of workspaces
+# It can be used to return a list of workspaces with basic info or full info
+class WorkspaceList(BaseModel):
+    workspaces: List[WorkspaceReadShort | WorkspaceReadFull]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "workspaces": [
+                    {
+                        "name": "Workspace 01",
+                        "description": "This is an example workspace",
+                        "owner": "true",
+                    },
+                    {
+                        "name": "Workspace 02",
+                        "description": "This is another example workspace",
+                        "owner": "false",
+                    },
+                ]
+            }
+        }
+
