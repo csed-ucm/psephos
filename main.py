87ff011443ebf709e5d4fd479c9fa32e7ba0904a
Michael Pisman <mpisman@ucmerced.edu>
2023-05-19 00:30:46 -0700

    Removed deprecated debug flag

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import app
 
 if __name__ == "__main__":
     # uvicorn.run(app, host="0.0.0.0", port=8000)
-    uvicorn.run("app.app:app", reload=True, debug=True, host="0.0.0.0", port=8000)
+    # NOTE:  You must pass the application as an import string to enable 'reload' or 'workers'.
+    uvicorn.run("app.app:app", reload=True, host="0.0.0.0", port=8000)
