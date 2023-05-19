7d459168de6e05cfa837842712f14fe4a6518049
Michael Pisman <mpisman@ucmerced.edu>
2023-05-19 00:21:27 -0700

    Moved List of document models to mongo_db.py Renamed get_user_db to get_account_db Moved create_link function to models/documents.py

--- app/mongo_db.py
+++ app/mongo_db.py
@@ -1,11 +1,9 @@
-from beanie import Document, Link
 from typing import AsyncGenerator
-from bson import DBRef
 import motor.motor_asyncio
 from fastapi_users.db import BeanieUserDatabase
-from app.models.user import User
+from app.models import documents as Documents
 from app.config import get_settings
-
+from app.account_manager import AccessToken
 
 settings = get_settings()
 
@@ -16,13 +14,15 @@ client = motor.motor_asyncio.AsyncIOMotorClient(
 mainDB = client.app
 
 
-async def get_user_db() -> AsyncGenerator:
-    yield BeanieUserDatabase(User)
+async def get_account_db() -> AsyncGenerator:
+    yield BeanieUserDatabase(Documents.Account)  # type: ignore
 
 
-# Create a link to the Document model
-async def create_link(document: Document) -> Link:
-    ref = DBRef(collection=document._document_settings.name,  # type: ignore
-                id=document.id)
-    link = Link(ref, User)
-    return link
+DOCUMENT_MODELS = [
+    AccessToken,
+    Documents.Resource,
+    Documents.Account,
+    Documents.Group,
+    Documents.Workspace,
+    Documents.Policy
+]
