0552ccbe2fc22c9222f5703e4aad3f039083b059
Michael Pisman <mpisman@ucmerced.edu>
2023-05-18 23:35:37 -0700

    Removed deprecated dependency method

--- app/actions/account.py
+++ app/actions/account.py
@@ -1,57 +1,20 @@
-from beanie.operators import In
-from app.models.group import Group
-from app.models.account import Account
-from app.models.resource import ResourceID
-from pydantic import EmailStr
-from app.schemas.group import GroupReadShort, GroupList
-from app.exceptions.account import AccountNotFound
+# Account actions
+from app.models.documents import Account, ResourceID
+from app.account_manager import current_active_user
+from app.exceptions import account as AccountExceptions
 
 
-# Get all groups that a account is a member of with the account's role in that group
-async def get_account_groups(account: Account) -> GroupList:
-    """Get all groups that a account is a member of.
-    The function returns a list of dictionaries with role as a key and and a list of groups as a value.
+# Delete account
+async def delete_account(account_id: ResourceID | None) -> None:
+    account: Account = await Account.get(account_id) if account_id else current_active_user.get()  # type: ignore
 
-    Args:
-        account (Account): Account object with list of group ids
+    # Check if account exists
+    if not account and account_id:
+        raise AccountExceptions.AccountNotFound(account_id)
 
-    Returns:
-        GroupList: Model with the list of groups that account belongs to and the account's role in that group.
-    """
-    # groups = await Group.find(In(Group.id, account.groups)).to_list()
-    result = []
-    # for group in groups:
-    #     if account.id == group.owner:
-    #         result.append(GroupReadShort(name=group.name, role="owner"))
-    #     elif account.id in group.admins:
-    #         result.append(GroupReadShort(name=group.name, role="admin"))
-    #     elif account.id in group.members:
-    #         result.append(GroupReadShort(name=group.name, role="account"))
+    # Delete account
+    await Account.delete(account)
 
-    group_list = GroupList(groups=result)
-    return group_list
-
-
-# Check if account exists in the database
-async def check_account_exists(account: Account | ResourceID | EmailStr) -> Account:
-    """Check if account exists in the database
-
-    Args:
-        account (Account): Account object
-
-    Returns:
-        Account: returns Account object of found account, raises a HTTP exception otherwise
-    """
-    if isinstance(account, Account):
-        found_account = await Account.get(account.id)
-    elif isinstance(account, ResourceID):
-        found_account = await Account.get(account)
-    elif isinstance(account, EmailStr):
-        found_account = await Account.find_one({"email": account})
-    else:
-        # Display error message with the type of the account object
-        raise TypeError("Invalid type for account", type(account))
-
-    if not found_account:
-        raise AccountNotFound(str(account))
-    return found_account
+    # Check if account was deleted
+    if await Account.get(account.id):
+        raise AccountExceptions.ErrorWhileDeleting(account.id)
