cd5600bc6f0579ec915e80513bc65e6ad3bf5746
Michael Pisman <mpisman@ucmerced.edu>
2023-05-18 23:57:41 -0700

    Removed deprecated member schemas Removed regex filter for names

--- app/schemas/group.py
+++ app/schemas/group.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel, Field, EmailStr
-from typing import Any, List, Optional
+from pydantic import BaseModel, Field
+from typing import Any, Optional
 from app.models.documents import ResourceID
-from app.utils.permissions import Permissions
 
 
 class Group(BaseModel):
@@ -14,6 +13,7 @@ class Group(BaseModel):
     policies: Optional[list]
 
 
+# Schema for the response with basic group info after creation
 class GroupShort(BaseModel):
     id: ResourceID
     name: str
@@ -27,11 +27,7 @@ class GroupList(BaseModel):
 
 # Schema for the request to create a new group
 class GroupCreateInput(BaseModel):
-    name: str = Field(
-        default="",
-        min_length=3,
-        max_length=50,
-        regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
+    name: str = Field(default="", min_length=3, max_length=50)
     description: str = Field(default="", title="Description", max_length=300)
 
     class Config:
@@ -52,81 +48,12 @@ class GroupCreateOutput(BaseModel):
 
 # Schema for the request to add a user to a group
 class GroupUpdateIn(BaseModel):
-    name: Optional[str] = Field(
-        default="",
-        min_length=3,
-        max_length=50,
-        regex="^[A-Z][A-Za-z]{2,}([ ]([0-9]+|[A-Z][A-Za-z]*))*$")
+    name: Optional[str] = Field(title="Name", min_length=3, max_length=50)
     description: Optional[str] = Field(default="", title="Description", max_length=300)
 
     class Config:
         schema_extra = {
             "example": {
                 "Description": "Updated description"
-            }
-        }
-
-
-# Schema for the response with basic member info
-class GroupMember(BaseModel):
-    email: EmailStr
-    first_name: str
-    last_name: str
-    role: str = Field(default="user", title="Role")
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "user@example.com",
-                "first_name": "John",
-                "last_name": "Doe",
-                "role": "user",
-            }
-        }
-
-
-# Temporary schema for the request to add a member to a workspace
-class AddMembers(BaseModel):
-    accounts: list[ResourceID]
-    # groups: list[ResourceID]
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "accounts": [
-                    "1a2b3c4d5e6f7g8h9i0j",
-                    "2a3b4c5d6e7f8g9h0i1j",
-                    "3a4b5c6d7e8f9g0h1i2j"
-                ],
-                # "groups": [
-                #     "4a5b6c7d8e9f0g1h2i3j",
-                #     "5a6b7c8d9e0f1g2h3i4j",
-                #     "6a7b8c9d0e1f2g3h4i5j"
-                # ]
-            }
-        }
-
-
-# Schema for the response with a list of members and their info
-class GroupReadMembers(BaseModel):
-    members: List[GroupMember]
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "members": [
-                    {
-                        "email": "jdoe@example.com",
-                        "first_name": "John",
-                        "last_name": "Doe",
-                        "role": "admin"
-                    },
-                    {
-                        "email": "jsmith@example.com",
-                        "first_name": "Jack",
-                        "last_name": "Smith",
-                        "role": "user"
-                    }
-                ]
             }
         }
