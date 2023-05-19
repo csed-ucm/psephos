61afe4b7dfad29b0058f531ca43d736c138fe92a
Michael Pisman <mpisman@ucmerced.edu>
2023-05-18 23:50:43 -0700

    Replaced old routed definition with new format Removed GET {user_id}/groups (deprecated) Modified delete_user to use action delete_user and follow general format

--- app/routes/account.py
+++ app/routes/account.py
@@ -1,57 +1,16 @@
-from typing import Any
-from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.responses import JSONResponse
-from app.actions.account import AccountActions
-from app.models.resource import ResourceID
-from app.models.account import User
-from app.models.account_manager import get_current_active_user
-from app.schemas.group import GroupList
-from app.utils.account import get_user_groups, check_user_exists
-from app.utils.colored_dbg import info
+from fastapi import APIRouter, status, HTTPException
+from app.actions import account as AccountActions
+from app.models.documents import ResourceID
+from app.exceptions.resource import APIException
+
 
 # APIRouter creates path operations for user module
-router = APIRouter(
-    prefix="/users",
-    tags=["Users"],
-    responses={404: {"description": "Not found"}},
-)
-
-current_active_user: Any = get_current_active_user()
-
-
-# List all groups that a user is a member of
-@router.get("/{user_id}/groups",
-            response_description="List all groups of user with id {user_id}",
-            responses={
-                   200: {"description": "List of groups.", "model": GroupList},
-                   404: {"description": "User not found"},
-                   })
-async def list_user_groups(user_id: ResourceID,
-                           user: User = Depends(current_active_user)) -> GroupList:
-    """
-        ## Delete current user account
-
-        This route, finds user by provided id, or raises a HTTP exception (User not found).
-        If user is found, lists all groups that a user is a member of, with the user's role in that group.
-
-        ### Request Query Parameters
-
-        **user_id**: id of the user whose groups are to be listed
-    """
-    found_user = await check_user_exists(user_id)
-    return await get_user_groups(found_user)
+router = APIRouter()
 
 
 # Delete current user account
-@router.delete("/me",
-               status_code=status.HTTP_204_NO_CONTENT,
-               response_description="The account has been deleted",
-               responses={
-                   204: {"description": "The account has been deleted"},
-                   404: {"description": "User not found"},
-                   500: {"description": "User was not deleted"}
-                   })
-async def delete_user(user: User = Depends(current_active_user)) -> JSONResponse:
+@router.delete("/me")
+async def delete_user(account_id: ResourceID | None = None):
     """
         ## Delete current user account
 
@@ -65,9 +24,8 @@ async def delete_user(user: User = Depends(current_active_user)) -> JSONResponse
 
         **204** - *The account has been deleted*
     """
-    await Account.delete(user)
-    info(f"User {user.id} deleted")
-    found_user = await User.find_one({"_id": user.id})
-    if found_user:
-        raise HTTPException(status_code=500, detail="User was not deleted")
-    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content={})
+    try:
+        await AccountActions.delete_account(account_id)
+        return status.HTTP_204_NO_CONTENT
+    except APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
