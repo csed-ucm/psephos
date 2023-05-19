7711dd526b1f958870d53a6454afbc9648e468a6
Michael Pisman <mpisman@ucmerced.edu>
2023-05-18 23:41:12 -0700

    Converted exceptions from HTTPException to custom class APIException

--- app/exceptions/resource.py
+++ app/exceptions/resource.py
@@ -1,92 +1,73 @@
-from fastapi import HTTPException, status
+from fastapi import status
 from app.models.documents import Account, Resource
 from app.utils.colored_dbg import print_warning
 from beanie import PydanticObjectId
 
 
-class NonUniqueName(HTTPException):
-    def __init__(self, resource: str, resource_name: str):
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
-                         detail=f"{resource} with name \"{resource_name}\" already exists")
+class APIException(Exception):
+    def __init__(self, code: int, detail: str):
+        self.code = code
+        self.detail = detail
 
     def __str__(self) -> str:
         print_warning(self.detail)
         return self.detail
-        # logger.warning(self.detail)
 
 
-class ErrorWhileCreating(HTTPException):
+class NonUniqueName(APIException):
     def __init__(self, resource: str, resource_name: str):
-        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        super().__init__(code=status.HTTP_400_BAD_REQUEST,
+                         detail=f"{resource} with name {resource_name} already exists")
+
+
+class ErrorWhileCreating(APIException):
+    def __init__(self, resource: str, resource_name: str):
+        super().__init__(code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                          detail=f"Error while creating {resource} {resource_name}")
 
-    def __str__(self) -> str:
-        print_warning(self.detail)
-        return self.detail
-        # logger.warning(self.detail)
 
-
-class ResourceNotFound(HTTPException):
+class ResourceNotFound(APIException):
     def __init__(self, resource: str, resource_id: PydanticObjectId):
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
+        super().__init__(code=status.HTTP_400_BAD_REQUEST,
                          detail=f"{resource} #{resource_id} does not exist")
 
-    def __str__(self) -> str:
-        print_warning(self.detail)
-        return self.detail
-        # logger.warning(self.detail)
 
-
-class ErrorWhileDeleting(HTTPException):
+class ErrorWhileDeleting(APIException):
     def __init__(self, resource: str, resource_id: PydanticObjectId):
-        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        super().__init__(code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                          detail=f"Error while deleting {resource} #{resource_id}")
-
-    def __str__(self) -> str:
-        print_warning(self.detail)
-        return self.detail
-        # logger.warning(self.detail)
 
 
 # Not authorized
-class UserNotAuthorized(HTTPException):
+class UserNotAuthorized(APIException):
     def __init__(self, account: Account, resource: str, action: str = "perform this action"):
-        super().__init__(status_code=status.HTTP_403_FORBIDDEN,
+        super().__init__(code=status.HTTP_403_FORBIDDEN,
                          detail=f"User {account.email} is not authorized to {action} in {resource}")
-
-    def __str__(self) -> str:
-        print_warning(self.detail)
-        return self.detail
 
 
 # Action not found
-class ActionNotFound(HTTPException):
+class ActionNotFound(APIException):
     def __init__(self, resource: str, action: str):
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
+        super().__init__(code=status.HTTP_400_BAD_REQUEST,
                          detail=f"Action {action} not found in {resource}")
-
-    def __str__(self) -> str:
-        print_warning(self.detail)
-        return self.detail
 
 
 # Invalid permission
-class InvalidPermission(HTTPException):
+class InvalidPermission(APIException):
     def __init__(self, permission: str):
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
+        super().__init__(code=status.HTTP_400_BAD_REQUEST,
                          detail=f"Invalid permission {permission}")
-
-    def __str__(self) -> str:
-        print_warning(self.detail)
-        return self.detail
 
 
 # User not a member of resource
-class UserNotMember(HTTPException):
+class UserNotMember(APIException):
     def __init__(self, resource: Resource, user: Account):
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
+        super().__init__(code=status.HTTP_400_BAD_REQUEST,
                          detail=f"User {user.email} is not a member of {resource.name} #{resource.id}")
 
-    def __str__(self) -> str:
-        print_warning(self.detail)
-        return self.detail
+
+# User already a member of resource
+class AddingExistingMember(APIException):
+    def __init__(self, resource: Resource, user: Account):
+        super().__init__(code=status.HTTP_400_BAD_REQUEST,
+                         detail=f"User {user.email} is already a member of {resource.name} #{resource.id}")
