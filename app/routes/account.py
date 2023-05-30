dada11f68ce4109650ca49a7644d1b67fe7ace7e
Michael Pisman <mpisman@ucmerced.edu>
2023-05-30 08:48:10 -0700

    Updated route to delete current active user Replaced default route to delete account by id

--- app/routes/account.py
+++ app/routes/account.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from app.actions import account as AccountActions
-from app.models.documents import ResourceID
 from app.exceptions.resource import APIException
+from app.models.documents import Account
+from app.dependencies import get_account
 
 
 # APIRouter creates path operations for user module
@@ -10,7 +11,7 @@ router = APIRouter()
 
 # Delete current user account
 @router.delete("/me")
-async def delete_user(account_id: ResourceID | None = None):
+async def delete_my_account():
     """
         ## Delete current user account
 
@@ -25,7 +26,30 @@ async def delete_user(account_id: ResourceID | None = None):
         **204** - *The account has been deleted*
     """
     try:
-        await AccountActions.delete_account(account_id)
+        await AccountActions.delete_account()
+        return status.HTTP_204_NO_CONTENT
+    except APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
+
+
+# Delete user account by id
+@router.delete("/{id}")
+async def delete_user(account: Account = Depends(get_account)):
+    """
+        ## Delete current user account
+
+        This route deletes the account of the currently logged in user.
+
+        ### Request body
+
+        - **user** - User object
+
+        ### Expected Response
+
+        **204** - *The account has been deleted*
+    """
+    try:
+        await AccountActions.delete_account(account)
         return status.HTTP_204_NO_CONTENT
     except APIException as e:
         raise HTTPException(status_code=e.code, detail=str(e))
