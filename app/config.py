5d51243b971652b1aa216ae0686d49a63631be2b
Michael Pisman <mike.pisman@gmail.com>
2023-06-22 23:37:12 -0700

    Added app config and updated app version

--- app/config.py
+++ app/config.py
@@ -2,12 +2,13 @@ from pydantic import BaseSettings
 from functools import lru_cache
 
 
-VERSION = "0.0.6"
+VERSION = "0.0.7"
 
 
 class Settings(BaseSettings):
-    app_name: str = "Polling App"
+    app_name: str = "University Polling API"
     app_version: str = VERSION
+    app_description: str = "An Open Source API for creating surveys and polls to assist in university research."
     admin_email: str = ""
     mongodb_url: str = "mongodb://localhost:27017"
 
