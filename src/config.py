b413dc02c0b73d06caec620cc87d8ebc6b6936b1
Michael Pisman <mike.pisman@gmail.com>
2023-06-27 23:03:13 -0700

    Added admin email and Fields for settings

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, EmailStr, Field
 from functools import lru_cache
 
 
@@ -6,11 +6,16 @@ VERSION = "0.0.7"
 
 
 class Settings(BaseSettings):
-    app_name: str = "University Polling API"
+    app_name: str = Field(default="University Polling API",
+                          title="App Name", description="The name of the API.")
     app_version: str = VERSION
-    app_description: str = "An Open Source API for creating surveys and polls to assist in university research."
-    admin_email: str = ""
-    mongodb_url: str = "mongodb://localhost:27017"
+    app_description: str = Field(default=("An Open Source API for creating surveys and polls "
+                                          "to assist in university research."),
+                                 title="App Description", description="A description of the API.")
+    admin_email: EmailStr = Field(default=EmailStr("admin@unipoll.cc"),
+                                  title="Admin Email", description="The email address of the admin of the API.")
+    mongodb_url: str = Field(default="mongodb://localhost:27017",
+                             title="MongoDB URL", description="The URL of the MongoDB database.")
     secrete_key: str = "secret"
 
     class Config:
