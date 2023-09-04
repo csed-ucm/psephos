d540f7fa22d0602a9a2e9b00411a89982a336197
Michael Pisman <mike.pisman@gmail.com>
2023-09-03 21:55:14 -0600

    fix: Fixed issue with mypy
    Fixed issue with mypy not parsing BaseSettings type

--- src/config.py
+++ src/config.py
@@ -1,14 +1,15 @@
 from pydantic import BaseSettings, EmailStr, Field
 from functools import lru_cache
+from setuptools_scm import get_version
 
 
-VERSION = "0.7.43"
+VERSION = get_version(root='..', relative_to=__file__)
 
 
-class Settings(BaseSettings):
+class Settings(BaseSettings):  # type: ignore
     app_name: str = Field(default="University Polling API",
                           title="App Name", description="The name of the API.")
-    app_version: str = VERSION
+    app_version: str = Field(default=VERSION, title="App Version", description="The version of the API.")
     app_description: str = Field(default=("An Open Source API for creating surveys and polls "
                                           "to assist in university research."),
                                  title="App Description", description="A description of the API.")
@@ -16,7 +17,7 @@ class Settings(BaseSettings):
                                   title="Admin Email", description="The email address of the admin of the API.")
     mongodb_url: str = Field(default="mongodb://localhost:27017",
                              title="MongoDB URL", description="The URL of the MongoDB database.")
-    secrete_key: str = "secret"
+    secrete_key: str = Field(default="secret", title="Secrete Key", description="The secrete key of the API.")
 
     class Config:
         env_file = ".env"
