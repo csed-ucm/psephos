08a964560fdc57106995614b07960ef939310a7d
Michael Pisman <mike.pisman@gmail.com>
2023-09-13 20:43:57 -0600

    feat: Added response scheme PollShort
    Changed PollList scheme to exclude questions and policies fields

--- src/schemas/poll.py
+++ src/schemas/poll.py
@@ -23,8 +23,27 @@ class Poll(BaseModel):
         }
 
 
+class PollShort(BaseModel):
+    id: ResourceID
+    name: str
+    description: str
+    published: bool
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "poll": {
+                    "id": "1a2b3c4d5e6f7g8h9i0j",
+                    "name": "Poll 01",
+                    "description": "This is an example poll",
+                    "published": True
+                }
+            }
+        }
+
+
 class PollList(BaseModel):
-    polls: list[Poll]
+    polls: list[PollShort]
 
     class Config:
         schema_extra = {
