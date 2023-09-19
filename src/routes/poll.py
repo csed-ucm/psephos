766fd63e8fb1f2c8c8b06fc2abe09a0be9575219
Michael Pisman <mike.pisman@gmail.com>
2023-09-19 17:25:41 -0600

    feat: Added routes to update and delete polls

--- src/routes/poll.py
+++ src/routes/poll.py
@@ -1,15 +1,14 @@
 # APIRouter creates path operations for user module
 from typing import Annotated, Literal
-from fastapi import APIRouter, Depends, Query, HTTPException
+from fastapi import APIRouter, Body, Depends, Query, HTTPException
 
 from src import dependencies as Dependencies
 from src.documents import Poll
-from src.account_manager import current_active_user
 from src.exceptions.resource import APIException
 from src.schemas import poll as PollSchemas
-from src.schemas import workspace as WorkspaceSchema
+from src.schemas import question as QuestionSchemas
+from src.schemas import policy as PolicySchema
 from src.actions import poll as PollActions
-from src.utils import permissions as Permissions
 
 open_router = APIRouter()
 router = APIRouter(dependencies=[Depends(Dependencies.check_poll_permission)])
@@ -20,43 +19,61 @@ query_params = list[Literal["all", "questions", "policies"]]
 
 # Get poll by id
 @router.get("/{poll_id}",
-            response_description="Get a group",
-            response_model=PollSchemas.Poll,
-            response_model_exclude_defaults=True,
+            response_description="Poll details",
+            response_model=PollSchemas.PollResponse,
             response_model_exclude_none=True)
 async def get_poll(poll: Poll = Depends(Dependencies.get_poll_model),
-                   include: Annotated[query_params | None, Query()] = None) -> PollSchemas.Poll:
+                   include: Annotated[query_params | None, Query()] = None):
     try:
-        account = current_active_user.get()
-        questions = []
-        policies = None
+        return await PollActions.get_poll(poll, list(include or []))
+    except APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
 
-        if include:
-            # Get the permissions(allowed actions) of the current user
-            permissions = await Permissions.get_all_permissions(poll, account)
-            # If "all" is in the list, include all resources
-            if "all" in include:
-                include = ["policies", "questions"]
-            # Fetch the resources if the user has the required permissions
-            if "questions" in include:
-                req_permissions = Permissions.PollPermissions["get_poll_questions"]  # type: ignore
-                if Permissions.check_permission(permissions, req_permissions) or poll.public:
-                    questions = (await PollActions.get_poll_questions(poll)).questions
-            if "policies" in include:
-                req_permissions = Permissions.PollPermissions["get_poll_policies"]  # type: ignore
-                if Permissions.check_permission(permissions, req_permissions):
-                    policies = (await PollActions.get_poll_policies(poll)).policies
 
-        workspace = WorkspaceSchema.WorkspaceShort(**poll.workspace.dict())  # type: ignore
+# Update poll details
+@router.patch("/{poll_id}",
+              response_description="Update Poll detail",
+              response_model=PollSchemas.PollResponse,
+              response_model_exclude_none=True)
+async def update_poll(poll: Poll = Depends(Dependencies.get_poll_model),
+                      data: PollSchemas.UpdatePollRequest = Body(...)):
+    try:
+        return await PollActions.update_poll(poll, data)
+    except APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
 
-        # Return the workspace with the fetched resources
-        return PollSchemas.Poll(id=poll.id,
-                                name=poll.name,
-                                description=poll.description,
-                                public=poll.public,
-                                published=poll.published,
-                                workspace=workspace,
-                                questions=questions,
-                                policies=policies)
+
+# Delete poll by id
+@router.delete("/{poll_id}",
+               response_description="Result of delete operation",
+               status_code=204)
+async def delete_poll(poll: Poll = Depends(Dependencies.get_poll_model)):
+    try:
+        return await PollActions.delete_poll(poll)
+    except APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
+
+
+# Get list of questions in a poll
+@router.get("/{poll_id}/questions",
+            response_description="Questions in a poll",
+            response_model=QuestionSchemas.QuestionList,
+            response_model_exclude_none=True)
+async def get_questions(poll: Poll = Depends(Dependencies.get_poll_model),
+                        include: Annotated[query_params | None, Query()] = None):
+    try:
+        return await PollActions.get_poll_questions(poll)
+    except APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
+
+
+@router.get("/{poll_id}/policies",
+            response_description="Policy list of a poll",
+            response_model=PolicySchema.PolicyList,
+            response_model_exclude_none=True)
+async def get_policies(poll: Poll = Depends(Dependencies.get_poll_model),
+                       include: Annotated[query_params | None, Query()] = None):
+    try:
+        return await PollActions.get_poll_policies(poll)
     except APIException as e:
         raise HTTPException(status_code=e.code, detail=str(e))
