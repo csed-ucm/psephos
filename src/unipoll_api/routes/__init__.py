39128024760eb4f28bef2abbcc051b47c6d3aea3
Michael Pisman <mike.pisman@gmail.com>
2023-10-29 20:23:55 -0600

    refactor: Replaced version specific function with one function for generating unique operation IDs based on provided API version

--- src/unipoll_api/routes/__init__.py
+++ src/unipoll_api/routes/__init__.py
@@ -7,16 +7,11 @@ from .swagger_docs import create_doc_router
 from .websocket import router as websocket_router
 
 
-# TODO: find a way to generate unique IDs for different versions with one function
-
-# Function to generate unique IDs for API v1 endpoints
-def generate_unique_id_v1(route: APIRoute):
-    return f"api-v1-{route.tags[0]}-{route.name}"
-
-
-# Function to generate unique IDs for API v2 endpoints
-def generate_unique_id_v2(route: APIRoute):
-    return f"api-v2-{route.tags[0]}-{route.name}"
+# Function to generate unique operation IDs for different API versions
+def generate_unique_id(version: int):
+    def func(route: APIRoute):
+        return f"api-v{version}-{route.tags[0]}-{route.name}"
+    return func
 
 
 # Function to create API Router
@@ -37,14 +32,14 @@ def create_router(app, default_version):
     # Add API v1 endpoints to the main router
     router.include_router(v1_router,
                           prefix="/v1",
-                          generate_unique_id_function=generate_unique_id_v1)
+                          generate_unique_id_function=generate_unique_id(1))
     # Add API v1 OpenAPI schemas for documentation
     api_versioning.add_api(v1_router, version=1)
 
     # Add API v2 endpoints to the main router
     router.include_router(v2_router,
                           prefix="/v2",
-                          generate_unique_id_function=generate_unique_id_v2)
+                          generate_unique_id_function=generate_unique_id(2))
     # Add API v2 OpenAPI schemas for documentation
     api_versioning.add_api(v2_router, version=2)
 
