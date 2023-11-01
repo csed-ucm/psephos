8b85dc2d36460eb17f6fadc3f3572e52e8393d48
Michael Pisman <mike.pisman@gmail.com>
2023-10-31 23:58:37 -0600

    refactor: Updated action parser
    Added filter_arguments function

--- src/unipoll_api/websocket_manager.py
+++ src/unipoll_api/websocket_manager.py
@@ -1,5 +1,9 @@
 from fastapi import WebSocket
+from typing import Any
+import inspect
 from unipoll_api.schemas.websocket import Message
+from unipoll_api import actions
+from unipoll_api.exceptions import websocket as WebSocketExceptions
 
 
 class WebSocketManager:
@@ -14,12 +18,35 @@ class WebSocketManager:
         self.active_connections.remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket) -> None:
-        await websocket.send_text(message)
+        await websocket.send_json(message)
 
     async def broadcast(self, message: str) -> None:
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection.send_json(message)
 
 
-def action_parser(message: Message) -> any:
-    return message.action
+def filter_arguments(action, data):
+    sig = inspect.signature(action)
+    required_args = []
+    for param in sig.parameters.values():
+        if param.kind == param.POSITIONAL_OR_KEYWORD and param.default == param.empty: 
+            required_args.append(param.name)
+
+    filtered_args = {}
+    for key in required_args:
+        value = data.get(key)
+        if not value:
+            raise WebSocketExceptions.ActionMissingRequiredArgs(action.__name__, required_args, list(data.keys()))
+        filtered_args[key] = value
+    return filtered_args
+
+
+functions = {name: func for name, func in inspect.getmembers(actions, inspect.isfunction)}
+
+
+async def action_parser(message: Message) -> Any:
+    action = functions.get(message.action)
+    if not action:
+        raise WebSocketExceptions.InvalidAction(message.action)
+    args = filter_arguments(action, message.data)
+    return (await action(**args)).model_dump()
