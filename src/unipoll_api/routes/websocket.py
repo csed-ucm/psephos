9788101bcdc7ab397ae0b45634f526086c624ebf
Michael Pisman <mike.pisman@gmail.com>
2023-11-01 21:04:12 -0600

    style: Added comments

--- src/unipoll_api/routes/websocket.py
+++ src/unipoll_api/routes/websocket.py
@@ -17,22 +17,33 @@ manager = WebSocketManager()
 
 @router.websocket("")
 async def open_websocket_endpoint(websocket: WebSocket, user: Account = Depends(websocket_auth)):
+    # Connect to the WebSocket
     await manager.connect(websocket)
+
+    # Check if user is authenticated
     if not user:
-        error = WebSocketExceptions.AuthenticationError
+        error = WebSocketExceptions.AuthenticationError()
         raise WebSocketException(error.code, error.detail)
 
+    # Set the active user
     await set_active_user(user)
-    # Send a welcome message
+
+    # Send a welcome message and log the connection
     Debug.info(f"{user.email} connected via websocket")
     await manager.send_personal_message(f"Welcome, {(user.first_name).capitalize()}", websocket)
+
+    # Wait for incoming messages
     try:
         while True:
             try:
+                # Receive a message from the client and parse it
                 data = await websocket.receive_json()
                 message = Message(action=data["action"], data=data.get("data"))
+                # Respond to the client with the result of the action
                 await manager.send_personal_message(await action_parser(message), websocket)
+            # Catch API exceptions and send them to the client
             except APIException as e:
                 await manager.send_personal_message({'error': e.detail}, websocket)
+    # Disconnect the client when the connection is closed
     except WebSocketDisconnect:
         await manager.disconnect(websocket)
