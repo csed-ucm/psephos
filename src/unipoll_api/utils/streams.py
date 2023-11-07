e6f3a8481d9ea510c2f1d7e801eae9053eba9430
Michael Pisman <mike.pisman@gmail.com>
2023-11-06 21:29:58 -0700

    feat: Add streaming endpoint and generator skeleton

---
+++ src/unipoll_api/utils/streams.py
@@ -0,0 +1,15 @@
+import asyncio
+from unipoll_api.documents import Resource
+from . import colored_dbg as Debug
+
+
+async def update_generator(resource: Resource):
+    i = 0
+    try:
+        while True:
+            i += 1
+            yield dict(data=i)
+            await asyncio.sleep(0.2)
+    except asyncio.CancelledError as e:
+        Debug.info("Disconnected from client (via refresh/close)")
+        raise e
