3e378e89a4c92e6b22db677a4b559dd4a9098100
Michael Pisman <mike.pisman@gmail.com>
2023-11-23 21:56:38 -0700

    feat: Added get member endpoint

--- src/unipoll_api/routes/v2/members.py
+++ src/unipoll_api/routes/v2/members.py
@@ -1,6 +1,6 @@
 
 from typing import Annotated
-from fastapi import APIRouter, Body, HTTPException, Query
+from fastapi import APIRouter, Body, Depends, HTTPException, Query
 from unipoll_api import actions as Actions
 from unipoll_api import documents as Documents
 from unipoll_api import schemas as Schemas
@@ -14,14 +14,17 @@ router = APIRouter()
 @router.get("", response_description="List of groups", response_model=Schemas.MemberSchemas.MemberList)
 async def get_members(workspace: Annotated[Documents.ResourceID | None, Query()] = None,
                       group: Annotated[Documents.ResourceID | None, Query()] = None):
-    if workspace:
-        resource = await Dependencies.get_workspace(workspace)
-    elif group:
-        resource = await Dependencies.get_group(group)
-    else:
-        raise Exceptions.ResourceExceptions.APIException(422, "Either Workspace or Group must be specified")
-
-    return await Actions.MembersActions.get_members(resource=resource)
+    try:
+        if workspace:
+            resource = await Dependencies.get_workspace(workspace)
+        elif group:
+            resource = await Dependencies.get_group(group)
+        else:
+            raise Exceptions.ResourceExceptions.APIException(422, "Either Workspace or Group must be specified")
+        
+        return await Actions.MembersActions.get_members(resource=resource)
+    except Exceptions.ResourceExceptions.APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
 
 
 # Add members to a workspace or group
@@ -39,5 +42,15 @@ async def add_members(input_data: Schemas.MemberSchemas.AddMembersRequest = Body
             raise Exceptions.ResourceExceptions.APIException(422, "Either Workspace or Group must be specified")
 
         return await Actions.MembersActions.add_members(resource, input_data.accounts)
+    except Exceptions.ResourceExceptions.APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
+
+
+@router.get("/{member_id}",
+            response_description="Member Information",
+            response_model=Schemas.MemberSchemas.Member)
+async def get_member(member = Depends(Dependencies.get_member)):
+    try:
+        return await Actions.MembersActions.get_member(member)
     except Exceptions.ResourceExceptions.APIException as e:
         raise HTTPException(status_code=e.code, detail=str(e))
