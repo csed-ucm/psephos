2e6744ca91029a268d9f826037c271362dd96563
Michael Pisman <mike.pisman@gmail.com>
2023-11-28 23:39:49 -0700

    refactor: Replaced plugin functions with PluginManger
    Created PluginManager class and turned all functions into methods, improved package discovery

--- src/unipoll_api/actions/__init__.py
+++ src/unipoll_api/actions/__init__.py
@@ -1,18 +1,21 @@
-from unipoll_api.plugins import run_plugins
-
 from functools import wraps
+
+from unipoll_api.plugin_manager import get_plugin_manager
+
+plugin_manager = get_plugin_manager()
 
 
 def plugins(f):
     @wraps(f)
     async def wrapper(*args, **kwds):
-        print("Action Wrapper")
-        print(f"Action: {f}")
-        print(f"Args: {args}")
-        print(f"Kwds: {kwds}")
-        print("\n")
-        res = await run_plugins(action=f(*args, **kwds))
-        print(f"\nWrapper Result: {res}")
+        # print("Action Wrapper")
+        # print(f"Action: {f}")
+        # print(f"Args: {args}")
+        # print(f"Kwds: {kwds}")
+        # print("\n")
+        
+        res = await plugin_manager.run_plugins(action=f(*args, **kwds))
+        # print(f"\nWrapper Result: {res}")
         
         return res
     return wrapper
