38db23c8343ff6b19d8f28d430896f071334a486
Michael Pisman <mike.pisman@gmail.com>
2023-12-06 22:09:05 -0700

    feat: Added endpoint to get member permissions
    This route returns list of all permissions a member has in a workspace and permissions for each group in the workspace that a member is a part of

--- src/unipoll_api/routes/v2/permissions.py
+++ src/unipoll_api/routes/v2/permissions.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, HTTPException
+from click import group
+from fastapi import APIRouter, HTTPException, Depends
+from unipoll_api import dependencies as Dependencies
+from unipoll_api.documents import Account, Workspace, Member
 from unipoll_api.schemas import PolicySchemas
 from unipoll_api.exceptions.resource import APIException
 from unipoll_api.actions import PermissionsActions
+from unipoll_api.utils import Permissions
+
 
 router = APIRouter()
 
@@ -24,5 +29,33 @@ async def get_workspace_permissions():
 async def get_group_permissions():
     try:
         return await PermissionsActions.get_group_permissions()
+    except APIException as e:
+        raise HTTPException(status_code=e.code, detail=str(e))
+
+
+@router.get("/members/{member_id}",
+            response_description="List of all member permissions")
+async def get_member_permissions(member: Member = Depends(Dependencies.get_member),
+                                 workspace: Workspace = Depends(Dependencies.get_workspace)):
+    try:
+        workspace_permissions = await Permissions.get_all_permissions(member, workspace)
+        group_permissions = {}
+        for group in member.groups:
+            group_permissions[group.id] = await Permissions.get_all_permissions(member, group)
+        
+        return {
+            "permissions": {
+                "workspace": {
+                    "id": workspace.id,
+                    "permissions": workspace_permissions,
+                },
+                "groups": [ {
+                        "id": group.id,
+                        "permissions": group_permissions[group.id]
+                    } for group in member.groups 
+                ]
+            }
+        }
+        
     except APIException as e:
         raise HTTPException(status_code=e.code, detail=str(e))
